6f7d0e658f921a4e2be2f50a9ff4af7298892a38
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 17:50:46 +0300

    feat(test): test successful signin

---
+++ tests/test_auth.py
@@ -0,0 +1,20 @@
+"""Test the user view funciton."""
+from json import loads, dumps
+
+from tests.base import BaseCase
+
+
+class TestAuth(BaseCase):
+    """User resource tests."""
+
+    def test_auth_user(self):
+        """Test create  user endpoint."""
+        
+        # test correct signin
+        response = self.client.post(
+            '/api/v1/users/signin',
+            data={'username': 'user1', 'password':'pass#123'})
+        self.assertEqual(200, response.status_code)
+        expected = {'message': 'User login successful.'}
+        self.assertEqual(expected['message'], loads(response.data)['message'])
+        self.assertTrue(loads(response.data)['token'])
