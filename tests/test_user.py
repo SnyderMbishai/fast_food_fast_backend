a15b493e9429fbfc38fcde9753a4f85d262ee668
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 18:04:34 +0300

    feat(tests): test already signed up

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,10 +9,16 @@ class TestUser(BaseCase):
 
     def test_create_user(self):
         """Test create  user endpoint."""
-        
+
         # test correct signup
         response = self.client.post('/api/v1/users/signup', data=self.user_data_1)
         self.assertEqual(201, response.status_code)
         expected = {'message': 'User registration successful'}
         self.assertEqual(expected['message'], loads(response.data)['message'])
         self.assertTrue(loads(response.data)['token'])
+
+        # test duplicate signup
+        response = self.client.post('/api/v1/users/signup', data=self.user_data_1)
+        self.assertEqual(400, response.status_code)
+        expected = {'message': 'Username/Email not available.'}
+        self.assertEqual(expected['message'], loads(response.data)['message'])
