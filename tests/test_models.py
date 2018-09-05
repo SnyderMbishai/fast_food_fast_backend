924eb3fdcd018c51896b2f3b0a9592da22b81715
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 17:09:32 +0300

    chore(tests): correct identation

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,26 +6,30 @@ from .base import BaseCase
 class TestModels(BaseCase):
     '''Class for testing the user model.'''
 
-    # Test saving a user.
-    result = self.user1.save()
-    expected = {
-        'username': 'user1',
-        'id': 1,
-        'roles':[],
-        'email': 'user1@email.com'}
-    self.assertDictEqual(result, expected)
+    def test_user(self):
+        '''Test user model.'''
+        
+        # Test saving a user.
+        result = self.user1.save()
+        expected = {
+            'username': 'user1',
+            'id': 1,
+            'roles':[],
+            'email': 'user1@email.com'}
+        self.assertDictEqual(result, expected)
 
-    # Test getting a user.
-    self.assertIsInstance(User.get(id=1), User)
+        # Test getting a user.
+        self.assertIsInstance(User.get(id=1), User)
 
-    # Test getting a user by key.
-    self.assertIsInstance(User.get_by_key(username='user1'), User)
+        # Test getting a user by key.
+        self.assertIsInstance(User.get_by_key(username='user1'), User)
 
-    # Test get all users.
-    self.assertIsInstance(User.get_all(), dict)
-    self.assertEqual(1, len(User.get_all()))
+        # Test get all users.
+        self.assertIsInstance(User.get_all(), dict)
+        self.assertEqual(1, len(User.get_all()))
 
-    # Test updating a user.
-    new_data = {'username': 'New Username'}
-    result = self.user1.update(new_data)
-    self.assertEqual(result['username'], new_data['username'])
+        # Test updating a user.
+        new_data = {'username': 'New Username'}
+        result = self.user1.update(new_data)
+        self.assertEqual(result['username'], new_data['username'])
+
