c079328434dc4dcd39d22aaa88b877fe951447d1
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 18:05:51 +0300

    feat(tests): test signing with invalid username

--- tests/test_user.py
+++ tests/test_user.py
@@ -22,3 +22,9 @@ class TestUser(BaseCase):
         self.assertEqual(400, response.status_code)
         expected = {'message': 'Username/Email not available.'}
         self.assertEqual(expected['message'], loads(response.data)['message'])
+
+        # test signup using invalid username
+        response = self.client.post('/api/v1/users/signup', data=self.user_data_2)
+        self.assertEqual(400, response.status_code)
+        expected = {'message': 'Invalid username.'}
+        self.assertEqual(expected['message'], loads(response.data)['message'])
