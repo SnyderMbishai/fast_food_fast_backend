338beeed9d56fea735927f5a62d3811d20228777
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 17:54:45 +0300

    feat(tests): test wrong password provided

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,7 +9,7 @@ class TestAuth(BaseCase):
 
     def test_auth_user(self):
         """Test create  user endpoint."""
-        
+
         # test correct signin
         response = self.client.post(
             '/api/v1/users/signin',
@@ -18,3 +18,11 @@ class TestAuth(BaseCase):
         expected = {'message': 'User login successful.'}
         self.assertEqual(expected['message'], loads(response.data)['message'])
         self.assertTrue(loads(response.data)['token'])
+
+        # test signin with wrong password
+        response = self.client.post(
+            '/api/v1/users/signin',
+            data={'username': 'user1', 'password':'pass#1234'})
+        self.assertEqual(401, response.status_code)
+        expected = {'message': 'Username/Password Invalid.'}
+        self.assertEqual(expected['message'], loads(response.data)['message'])
