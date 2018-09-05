35a223f9ca00c55429223d25630d281b4e816d7d
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 17:00:06 +0300

    feat(tests): test saving a user

---
+++ tests/test_models.py
@@ -0,0 +1,16 @@
+'''Test models.'''
+
+from api.models import Meal, Order, User
+from .base import BaseCase
+
+class TestModels(BaseCase):
+    '''Class for testing the user model.'''
+
+    # Test saving a user.
+    result = self.user1.save()
+    expected = {
+        'username': 'user1',
+        'id': 1,
+        'roles':[],
+        'email': 'user1@email.com'}
+    self.assertDictEqual(result, expected)
