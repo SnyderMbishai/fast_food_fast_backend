f896a31cb3e7f94b05aba0db6d871a7296ebf283
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 08:09:49 +0300

    feature(model): Initialize the user object

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 """Models and their methods."""
 
 db = {}
+
+class User(object):
+    tablename = 'users'
+    def __init__(self, username, password, email):
+        self.id = None
+        self.username = username
+        self.email = email
+        self.password = password
+        self.roles = []
