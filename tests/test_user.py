c92ce86b12bc01fde05ca591f4ce347f5b0862cc
SnyderMbishai <csmbishai@gmail.com>
2018-09-05 18:02:07 +0300

    feat(tests): test correct signup

---
+++ tests/test_user.py
@@ -0,0 +1,18 @@
+"""Test the user view funciton."""
+from json import loads, dumps
+
+from tests.base import BaseCase
+
+
+class TestUser(BaseCase):
+    """User resource tests."""
+
+    def test_create_user(self):
+        """Test create  user endpoint."""
+        
+        # test correct signup
+        response = self.client.post('/api/v1/users/signup', data=self.user_data_1)
+        self.assertEqual(201, response.status_code)
+        expected = {'message': 'User registration successful'}
+        self.assertEqual(expected['message'], loads(response.data)['message'])
+        self.assertTrue(loads(response.data)['token'])
