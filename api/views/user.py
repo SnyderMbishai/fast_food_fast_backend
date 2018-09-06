958fed4e6b9319c71e5876560db92c7f7f6257f5
SnyderMbishai <csmbishai@gmail.com>
2018-09-06 12:58:55 +0300

    feat(views): get user details

---
+++ api/views/user.py
@@ -0,0 +1,13 @@
+'''User resource.'''
+
+import re
+from flask_restful import Resource, reqparse
+from api.models import User
+
+class UserResource(Resource):
+    '''Class for handling user registration.'''
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', required=True, type=str, help='Username (str) is required.')
+    parser.add_argument('email', required=True, type=str, help='Email (str) is required.')
+    parser.add_argument('password', required=True, type=str, help='Password (str) is required.')
