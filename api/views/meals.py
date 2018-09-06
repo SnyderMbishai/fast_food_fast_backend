68a3cf747003b8568a9dd08b6eeca2c0f9c3303c
SnyderMbishai <csmbishai@gmail.com>
2018-09-06 14:16:00 +0300

    feat(views): create a meal

--- api/views/meals.py
+++ api/views/meals.py
@@ -14,3 +14,18 @@ class MealResource(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('name', required=True, type=str, help='Name (str) is required.')
     parser.add_argument('price', required=True, type=int, help='Price (int) is required.')
+
+    def post(self):
+        '''Create a new meal.'''
+        
+        arguments = MealResource.parser.parse_args()
+        name = arguments.get('name')
+        price = arguments.get('price')
+
+        meal_exists = Meal.get_by_key(name=name)
+        if meal_exists:
+            return {'message': 'Meal with that name already exists.'}, 202
+        meal = Meal(name=name, price=price)
+        meal = meal.save()
+
+        return {'message': 'Meal successfully added.', 'meal': meal}, 201
