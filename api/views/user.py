b9a32f36f2e67f55bf326b1f4c39eec861373869
SnyderMbishai <csmbishai@gmail.com>
2018-09-06 13:02:08 +0300

    feat(views): set variables

--- api/views/user.py
+++ api/views/user.py
@@ -11,3 +11,11 @@ class UserResource(Resource):
     parser.add_argument('username', required=True, type=str, help='Username (str) is required.')
     parser.add_argument('email', required=True, type=str, help='Email (str) is required.')
     parser.add_argument('password', required=True, type=str, help='Password (str) is required.')
+
+    def post(self):
+        '''Create new user.'''
+        
+        arguments = UserResource.parser.parse_args()
+        password = arguments.get('password')
+        email = arguments.get('email')
+        username = arguments.get('username')
