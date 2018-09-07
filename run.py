9c69d38c16fa30978e11ccd40686df31ff99bac5
SnyderMbishai <csmbishai@gmail.com>
2018-09-07 23:37:31 +0300

    Update host for heroku

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from main import create_app
 
 configuration = os.getenv('APP_SETTINGS')
 app = create_app(configuration)
-app.run(debug=True, host='127.0.0.1', port=5000)
+app.run(debug=True, host='0.0.0.0', port=5000)
