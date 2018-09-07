7e42acd13b18b78a61851ca387c540464b9df9e1
Snyder Chebet Mbisha <smbisha@STANDARDMEDIA.CO.KE>
2018-09-07 11:38:15 +0300

    feat(set-up): create app before running

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-"""Run the application."""
-from main import app
+'''Run the application.'''
+import os
 
+from main import create_app
+
+configuration = os.getenv('APP_SETTINGS')
+app = create_app(configuration)
 app.run(debug=True, host='127.0.0.1', port=5000)
