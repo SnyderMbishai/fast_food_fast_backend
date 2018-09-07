eee0f1a06d1c02c61b5ef917e181b18959b40d89
Snyder Chebet Mbisha <smbisha@STANDARDMEDIA.CO.KE>
2018-09-07 16:50:34 +0300

    feat(decorators): add decorator for login-required

---
+++ api/helpers/decorators.py
@@ -0,0 +1,19 @@
+'''Decorators to implement authorization.'''
+
+from functools import wraps
+from flask import request
+from api.models import User
+
+
+def login_required(func):
+    '''Check if user has a valid token.'''
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        authorization_header = request.headers.get('Authorization')
+        if authorization_header:
+            access_token = authorization_header.split(' ')[1]
+            roles = User.decode_token(access_token)['roles']
+            if ('user' in roles):
+                return func(*args, **kwargs)
+        return {'message': 'Ensure you have an authorization header.'}, 400
+    return decorated
