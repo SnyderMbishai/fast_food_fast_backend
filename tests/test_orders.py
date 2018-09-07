10f871148ce9d5091f82fb5ce445c281d36f78e9
Snyder Chebet Mbisha <smbisha@STANDARDMEDIA.CO.KE>
2018-09-07 20:20:11 +0300

    chore(tests): format code

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -17,57 +17,50 @@ class TestOrderResource(BaseCase):
         # Request data.
         valid_order_data = dumps({"user_id": 1, "meal_dict": {1: 3}})
         invalid_order_data = dumps({"user_id": "a", "meal_dict": {1: 3}})
-        
         # Get user token to create header.
         token = self.get_user_token()
         headers = {'Authorization': 'Bearer {}'.format(token)}
-        
         # Using valid data.
         response = self.client.post(
-            ORDERS_URL, data=valid_order_data, headers=headers)        
+            ORDERS_URL, data=valid_order_data, headers=headers)
         # Check status code is 201
         self.assertEqual(response.status_code, 201)
-        expected = 'Order has been created successfully.'        
+        expected = 'Order has been created successfully.'
         # Check correct message returned.
         self.assertEqual(expected, loads(response.data)['message'])
-        
         # Check order create with invalid user_id.
         response = self.client.post(
-            ORDERS_URL, data=invalid_order_data, headers=headers)        
+            ORDERS_URL, data=invalid_order_data, headers=headers)
         # Check status code is 400.
         self.assertEqual(response.status_code, 400)
-        expected = 'user_id (int) is required.'        
+        expected = 'user_id (int) is required.'
         # Check correct message returned.
         self.assertEqual(expected, loads(response.data)['message'])
-        
         # Check order create with invalid meal_dict.
         invalid_order_data = dumps({"user_id": 1, 'meal_dict': []})
         response = self.client.post(
-            ORDERS_URL, data=invalid_order_data, headers=headers)        
+            ORDERS_URL, data=invalid_order_data, headers=headers)
         # Check status code is 400.
         self.assertEqual(response.status_code, 400)
-        expected = 'meal_dict (dict) is required.'        
+        expected = 'meal_dict (dict) is required.'
         # Check correct message returned.
         self.assertEqual(expected, loads(response.data)['message'])
-        
         # Create order for non-existent meal.
         valid_order_data = dumps({'user_id': 2, 'meal_dict': {2: 3}})
         response = self.client.post(
-            ORDERS_URL, data=valid_order_data, headers=headers)        
+            ORDERS_URL, data=valid_order_data, headers=headers)
         # Check status code is 400.
         self.assertEqual(response.status_code, 400)
         expected = 'Meal 2 does not exist.'
         self.assertEqual(expected, loads(response.data)['message'])
-        
         # Place order with invalid quantity.
         valid_order_data = dumps({'user_id': 2, 'meal_dict': {1: 'b'}})
         response = self.client.post(
-            ORDERS_URL, data=valid_order_data, headers=headers)        
+            ORDERS_URL, data=valid_order_data, headers=headers)
         # Check status code is 400.
         self.assertEqual(response.status_code, 400)
         expected = 'Meal quantities should be integers.'
         self.assertEqual(expected, loads(response.data)['message'])
-        
         # Place order with invalid meal_id.
         valid_order_data = dumps({'user_id': 2, 'meal_dict': {'a': 1}})
         response = self.client.post(
@@ -84,41 +77,35 @@ class TestOrderResource(BaseCase):
         admin_token = self.get_admin_token()
         user_header = {'Authorization': 'Bearer {}'.format(user_token)}
         admin_header = {'Authorization': 'Bearer {}'.format(admin_token)}
-        
         # Add an extra user.
         self.user2 = User(
             username='user2', email='user2@email.com', password='pass#123')
         self.user2.save()
         self.order2 = Order(2, {1: 4})
         self.order2.save()
-        
         # User request get one.
         response = self.client.get(ORDER_URL, headers=user_header)
         self.assertEqual(response.status_code, 200)
         expected = 'Order found.'
         self.assertEqual(loads(response.data)['message'], expected)
         self.assertTrue(loads(response.data)['order'])
-        
         # User request get all.
         response = self.client.get(ORDERS_URL, headers=user_header)
         self.assertEqual(response.status_code, 200)
         expected = 'Orders found.'
         self.assertEqual(loads(response.data)['message'], expected)
         self.assertEqual(len(loads(response.data)['orders']), 1)
-        
         # Admin request.
         response = self.client.get(ORDERS_URL, headers=admin_header)
         self.assertEqual(response.status_code, 200)
         expected = 'Orders found.'
         self.assertEqual(loads(response.data)['message'], expected)
         self.assertEqual(len(loads(response.data)['orders']), 2)
-        
         # User 1 cannnot see user 2's order.
         response = self.client.get('/api/v1/orders/2', headers=user_header)
         self.assertEqual(response.status_code, 403)
         expected = 'You do not have permission to see this order.'
         self.assertEqual(loads(response.data)['message'], expected)
-        
         # Returns 404 if order does not exist.
         response = self.client.get('/api/v1/orders/4', headers=user_header)
         self.assertEqual(response.status_code, 404)
@@ -150,13 +137,11 @@ class TestOrderResource(BaseCase):
 
         admin_token = self.get_admin_token()
         headers = {"Authorization": "Bearer {}". format(admin_token)}
-        
         # Admin can mark as completed.
         response = self.client.patch(ORDER_URL, headers=headers)
         self.assertEqual(response.status_code, 200)
         expected = 'Order 1 has been completed.'
         self.assertEqual(loads(response.data)['message'], expected)
-       
         # Mark a non-existent order.
         response = self.client.patch('/api/v1/orders/3', headers=headers)
         self.assertEqual(response.status_code, 404)
@@ -165,24 +150,21 @@ class TestOrderResource(BaseCase):
 
     def test_admin_can_decline_accept_order(self):
         '''Test admin declining order.'''
-
+        
         admin_token = self.get_admin_token()
         headers = {"Authorization": "Bearer {}". format(admin_token)}
-        
         # Test can accept
         response = self.client.patch(
             ACCEPT_URL, data=dumps({'accepted': True}), headers=headers)
         self.assertEqual(response.status_code, 200)
         expected = 'Order 1 has been accepted.'
         self.assertEqual(loads(response.data)['message'], expected)
-        
         # Test can decline.
         response = self.client.patch(
             ACCEPT_URL, data=dumps({'accepted': False}), headers=headers)
         self.assertEqual(response.status_code, 200)
         expected = 'Order 1 has been declined.'
         self.assertEqual(loads(response.data)['message'], expected)
-        
         # Test cannot accept/decline non existent.
         response = self.client.patch(
             '/api/v1/orders/accept/3',
@@ -192,14 +174,12 @@ class TestOrderResource(BaseCase):
         self.assertEqual(loads(response.data)['message'], expected)
         self.order1.completed = True
         self.order1.save()
-        
         # Test cannot edit an already completed oredr.
         response = self.client.patch(
             ACCEPT_URL, data=dumps({'accepted': False}), headers=headers)
         self.assertEqual(response.status_code, 202)
         expected = 'This order has already been completed.'
         self.assertEqual(loads(response.data)['message'], expected)
-        
         # Test accepted is boolean.
         response = self.client.patch(
             ACCEPT_URL, data=dumps({'accepted': 'p'}), headers=headers)
