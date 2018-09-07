e70e2e21a5b5600a886425449531175af5092ec2
Snyder Chebet Mbisha <smbisha@STANDARDMEDIA.CO.KE>
2018-09-07 16:52:20 +0300

    feat(decorators): add decorator for admin required

--- api/helpers/decorators.py
+++ api/helpers/decorators.py
@@ -16,4 +16,17 @@ def login_required(func):
             if ('user' in roles):
                 return func(*args, **kwargs)
         return {'message': 'Ensure you have an authorization header.'}, 400
+    return decorated    
+
+def admin_required(func):
+    '''Check if user has a valid admin token.'''
+    
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        authorization_header = request.headers.get('Authorization')
+        access_token = authorization_header.split(' ')[1]
+        roles = User.decode_token(access_token)['roles']
+        if ('admin' in roles):
+            return func(*args, **kwargs)
+        return {'message': 'This action requires an admin token.'}, 403
     return decorated
