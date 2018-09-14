44d90ef0733abec616228779cc3f158ad1747109
SnyderMbishai <csmbishai@gmail.com>
2018-09-14 13:55:08 +0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -36,6 +36,6 @@ def create_app(configuration):
     
     @app.route("/")
     def docs():
-        return render_template(".api/templates/documentation.html")
+        return render_template("api/documentation.html")
     
     return app
