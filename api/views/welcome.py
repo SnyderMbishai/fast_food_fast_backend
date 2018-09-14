44ac0a8ece3ad7805fd4b73966d54dbf691c633a
Snyder Chebet Mbisha <smbisha@STANDARDMEDIA.CO.KE>
2018-09-14 15:51:50 +0300

    feat(views): render template

--- api/views/welcome.py
+++ api/views/welcome.py
@@ -1,12 +1,9 @@
 """Welcome resource."""
-from flask_restful import Resource
+# from flask_restful import Resource
 from flask import render_template
 
+@app.route('/')
+def docs():
+    """Display template."""
 
-class WelcomeResource(Resource):
-    """Displays welcome message."""
-
-    def get(self):
-        """Display welcome message."""
-        # return {'message': 'Welcome to Fast Food Fast.'}, 200
-        return render_template("documentation.html")
+    return render_template("documentation.html")
