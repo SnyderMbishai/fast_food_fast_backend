c174f90bff151190de9423f17300179818785e6d
SnyderMbishai <csmbishai@gmail.com>
2018-09-25 15:27:43 +0300

    feat(helpers): add superuser decorator

--- api/helpers/decorators.py
+++ api/helpers/decorators.py
@@ -25,9 +25,26 @@ def admin_required(func):
     @wraps(func)
     def decorated(*args, **kwargs):
         authorization_header = request.headers.get('Authorization')
-        access_token = authorization_header.split(' ')[1]
-        roles = User.decode_token(token=access_token)['roles']
-        if ('admin' in roles):
-            return func(*args, **kwargs)
-        return {'message': 'This action requires an admin token.'}, 403
+        if authorization_header:
+            access_token = authorization_header.split(' ')[1]
+            roles = User.decode_token(token=access_token)['roles']
+            if ('admin' in roles):
+                return func(*args, **kwargs)
+            return {'message': 'This action requires an admin token.'}, 403
+        return{'message':'Ensure you have an authorization header.'}
+    return decorated
+
+def super_user_required(func):
+    '''Check whether user is a super admin'''
+    @wraps(func)
+
+    def decorated(*args, **kwargs):
+        authorization_header = request.headers.get('Authorization')
+        if authorization_header:
+            access_token = authorization_header.split(' ')[1]
+            roles = User.decode_token(token=access_token)['roles']
+            if 'superuser' in roles:
+                return func(*args, **kwargs)
+            return {'message': 'This action requires a superuser token.'}, 403
+        return{'message':'Ensure you have an authorization header.'}                                
     return decorated
