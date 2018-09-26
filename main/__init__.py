b37fccbe4d83fcdcd39941f24fcb4e34df0a79d5
SnyderMbishai <csmbishai@gmail.com>
2018-09-27 02:24:26 +0300

    chore(main): add blueprint

--- main/__init__.py
+++ main/__init__.py
@@ -1,26 +1,32 @@
 '''Create application.'''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from flask_restful import Api
 
 from main.config import configurations
+#v1
 from api.v1.views.welcome import WelcomeResource
 from api.v1.views.user import UserResource
 from api.v1.views.auth import AuthResource
 from api.v1.views.meals import MealResource
 from api.v1.views.orders import OrderResource, OrderManagement
 from api.v1.views.manage_user import ManageUsersResource
+#v2
+
 
 
 def create_app(configuration):
     '''Create the flask app.'''
     
     app = Flask(__name__)
+    api_blueprint = Blueprint('api', __name__)
+    api = Api(api_blueprint)
     app.config.from_object(configurations[configuration])
     app.url_map.strict_slashes = False
     app_context = app.app_context()
     app_context.push()
-    api = Api(app)
+    # api = Api(app)
+    
     # api.add_resource(
     #     WelcomeResource, '/', '/api/v1')
     api.add_resource(
@@ -35,6 +41,13 @@ def create_app(configuration):
         OrderManagement, '/api/v1/orders/accept/<int:order_id>')
     api.add_resource(
         ManageUsersResource, '/api/v1/users/manage/<int:user_id>')
+
+    #v2 urls
+    # api.add_resource()
+    # api.add_resource()
+
+
+    app.register_blueprint(api_blueprint)
     
     @app.route("/")
     def docs():
