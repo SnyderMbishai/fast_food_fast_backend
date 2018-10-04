9fb41d0c2a226d3c5adb8105e2dcdf1f06df6878
SnyderMbishai <csmbishai@gmail.com>
2018-10-04 16:26:08 +0300

    chore(set-up): update file

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn -b 0.0.0.0:$PORT run:app
+web: gunicorn --bind 0.0.0.0:${PORT}
+web: python run.py
--- run.py
+++ run.py
@@ -22,10 +22,10 @@ else:
     pass
 
 # run application
-CONFIGURATION = os.getenv('APP_SETTINGS')
-app = create_app(CONFIGURATION)
-PORT = os.getenv('PORT')
-app.run(debug=True, host='127.0.0.1', port=PORT)
+configuration = os.getenv('APP_SETTINGS')
+app = create_app(configuration)
+port = os.getenv('PORT')
+app.run(debug=True, host='0.0.0.0', port=port)
 
 # if CONFIGURATION != 'production':
 #     PORT = os.getenv('PORT')
